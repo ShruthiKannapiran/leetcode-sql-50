4586ac599db688d3e4ece9ef70a9e348ded5ca75
shruthikannapiran <shruthikannapiran@gmail.com>
2026-02-13 14:31:35 -0600

    Format Correction

--- Recyclable and Low Fat Products.sql
+++ Recyclable and Low Fat Products.sql
@@ -1,2 +1,9 @@
-# Write your MySQL query statement below
-select product_id from products where low_fats='Y' and recyclable='Y';
+-- Problem: Recyclable and Low Fat Products
+-- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+-- Difficulty: Easy
+
+
+select product_id 
+from products 
+where low_fats='Y' 
+   and recyclable='Y';
